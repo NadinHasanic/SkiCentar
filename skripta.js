ab846c236676b3752b48c713bc096ded3e54e89b
NadinHasanic <nhasanic1@etf.unsa.ba>
2016-03-29 21:31:25 +0200

    Spirala 2.2
    sve osim roda imenica i dodan opis za multiple field validaciju

--- skripta.js
+++ skripta.js
@@ -4,11 +4,26 @@ function crosValidacija(){
 	var mail = document.getElementById("email").value;
 	var adr = document.getElementById("adresa").value;
 	
-	if(valEmail(mail) && valAdresa(adr) ){ alert("oba valja"); return true; }
-	else if(valEmail(mail) && adr == "" ) { alert("mail valja"); return true; }
-	else if(valAdresa(adr) && mail == "") { alert("adresa valja"); return true; }
+	if(valCrosEmail() && valCrosAdresa() ){ alert("oba valja"); return true; }
+	else if(valCrosEmail() && adr == "" ) { alert("mail valja"); return true; }
+	else if(valCrosAdresa() && mail == "") { alert("adresa valja"); return true; }
 	else {alert("ne valja"); return false; }
 	
+}
+
+function valCrosEmail(){     /* valCros  dvije funkcije koriste se da se ne mijenja stalno boja bordera*/
+	var mail = document.getElementById("email").value;	
+	var reg = /^[A-Za-z0-9._-]+[@]{1}[a-z]+\.[a-z]{2,4}$/;
+	var t = reg.test(mail);
+	return t;
+}
+
+
+function valCrosAdresa(){
+	var adr = document.getElementById("adresa").value;
+	var reg = /^[A-Z]{1}[a-zčćžđš]+\s[0-9]+$/;
+	var a = reg.test(adr);
+	return a;
 }
 
 function valEmail(){
@@ -21,6 +36,10 @@ function valEmail(){
 	}
 	else {
 		document.getElementById("email").style.border = "2px solid red";
+	}
+	if(valCrosAdresa() && mail == "") {            /* ovaj dio se koristi za multiple field */
+		document.getElementById("email").style.border = "2px solid white";
+		t=true;
 	}
 	return t;
 }
@@ -48,6 +67,10 @@ function valAdresa(){
 	}
 	else{
 		document.getElementById("adresa").style.border = "2px solid red" ;
+	}
+	if(valCrosEmail() && adr == "") {                   /* ovaj dio se koristi za multiple field */
+		document.getElementById("adresa").style.border = "2px solid white";
+		a=true;
 	}
 	return a;
 }
@@ -83,4 +106,100 @@ function resetValidacije(){
 
 /*  ################### kraj VALIDACIJA-----------##########################3   */
 
+/*  POSTAVLJANJE NOVOSTI I ODABIR NOVOSTI ZA PRIKAZ  */
+
+function setNews(){
+	var nizVremena = document.getElementsByClassName("newsHideTime");
+	nizVremena[0].value = new Date("2016-03-29 20:00:00");
+	nizVremena[1].value = new Date("2016-03-29 19:00:00");
+	nizVremena[2].value = new Date("2016-03-29 12:00:00");
+	nizVremena[3].value = new Date("2016-03-27 12:00:00");
+	nizVremena[4].value = new Date("2016-03-26 12:00:00");
+	nizVremena[5].value = new Date("2016-03-21 12:00:00");
+	nizVremena[6].value = new Date("2016-03-17 12:00:00");
+	nizVremena[7].value = new Date("2016-03-07 12:00:00");
+	nizVremena[8].value = new Date("2016-03-05 12:00:00");
+	nizVremena[9].value = new Date("2016-03-03 12:00:00");
+	nizVremena[10].value = new Date("2016-02-03 12:00:00");
+	nizVremena[11].value = new Date("2016-01-02 12:00:00");
+
+	postNews();
+}
+function postNews(){
+	var sad = new Date().getTime()/1000;
+	var nizVremena = document.getElementsByClassName("newsHideTime");
+	var nizPrikaza = document.getElementsByClassName("newsTime");
+	var i;
+	for (i = 0; i < nizPrikaza.length; i++){
+		var d = new Date (nizVremena[i].value);
+		var tad = d.getTime()/1000;
+		if((sad - tad) < 60)                               { nizPrikaza[i].value = "Novost je objavljena prije "+Math.round(sad - tad ) +" sekundi"; }
+		if((sad - tad) < 3600 && (sad - tad) > 60 )        { nizPrikaza[i].value = "Novost je objavljena prije "+Math.round((sad - tad )/60) +" minute"; }
+		if((sad - tad) < 86400 && (sad - tad) > 3600)      { nizPrikaza[i].value = "Novost je objavljena prije "+Math.round((sad - tad )/3600) +" sata"; }
+		if((sad - tad) < 604800 && (sad - tad) > 86400 )   { nizPrikaza[i].value = "Novost je objavljena prije "+Math.round((sad - tad )/86400) +" dana"; }
+		if((sad - tad) < 2592000 && (sad - tad) > 604800 ) { nizPrikaza[i].value = "Novost je objavljena prije "+Math.round((sad - tad )/604800) +" sedmica"; }
+		else if((sad - tad) > 2592000){
+			nizPrikaza[i].value = "Novost je objavljena  "+(d.getDay()+1)+"-"+(d.getMonth()+1)+"-"+d.getFullYear();
+		}
+		
+	}
+}
+
+function prikaziDanas(){
+	var sad = new Date().getTime()/1000;
+	var nizVremena = document.getElementsByClassName("newsHideTime");
+	var nizNovosti = document.getElementsByClassName("news");
+	var i;
+	for (i = 0; i < nizNovosti.length; i++){
+		var d = new Date (nizVremena[i].value);
+		var tad = d.getTime()/1000;
+			if((sad - tad) > 86400) { 
+				nizNovosti[i].style.display = "none";
+			}
+			else {
+				nizNovosti[i].style.display = "block";
+			}
+	}
+}
+
+function prikaziSedmica(){
+	var sad = new Date().getTime()/1000;
+	var nizVremena = document.getElementsByClassName("newsHideTime");
+	var nizNovosti = document.getElementsByClassName("news");
+	var i;
+	for (i = 0; i < nizNovosti.length; i++){
+		var d = new Date (nizVremena[i].value);
+		var tad = d.getTime()/1000;
+			if((sad - tad) > 604800) { 
+				nizNovosti[i].style.display = "none";
+			}
+			else {
+				nizNovosti[i].style.display = "block";
+			}
+	}
+}
+
+function prikaziMjesec(){
+	var sad = new Date().getTime()/1000;
+	var nizVremena = document.getElementsByClassName("newsHideTime");
+	var nizNovosti = document.getElementsByClassName("news");
+	var i;
+	for (i = 0; i < nizNovosti.length; i++){
+		var d = new Date (nizVremena[i].value);
+		var tad = d.getTime()/1000;
+			if((sad - tad) > 2592000) { 
+				nizNovosti[i].style.display = "none";
+			}
+			else {
+				nizNovosti[i].style.display = "block";
+			}
+	}
+}
+
+function prikaziSve(){
+	var nizNovosti = document.getElementsByClassName("news");
+	for (i = 0; i < nizNovosti.length; i++){
+		nizNovosti[i].style.display = "block";
+	}
+}
 
